373e3afa81984ae8fedeb175e59dbc7f53eae2ee
sakuramochi <sakuramochi.mochi.0@gmail.com>
2017-11-05 01:26:51 +0900

    全体をmain()関数内に移動。--debug 引数を追加してデバッグを可能にする。

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import datetime
 import io
 import tempfile
@@ -6,26 +7,38 @@ from PIL import Image
 from get_tweepy import get_api
 
 
-tmp_path = tempfile.mktemp('.png')
-url = 'http://cafereserve.animate.co.jp/vacancy?group_id=11166'
+def main():
+    tmp_path = tempfile.mktemp('.png')
+    url = 'http://cafereserve.animate.co.jp/vacancy?group_id=11166'
 
-api = get_api('rl_cafe_reserve')
-br = PhantomJS()
+    # open reserve page
+    br.get(url)
 
-# open reserve page
-br.get(url)
+    # adjust prresentation
+    br.set_window_size(1024, 1500)
+    br.execute_script("$('.timeTableData').css('overflow-x', 'hidden')")
 
-# adjust prresentation
-br.set_window_size(1024, 1500)
-br.execute_script("$('.timeTableData').css('overflow-x', 'hidden')")
+    # save screenshot
+    img = Image.open(io.BytesIO(br.get_screenshot_as_png()))
+    crop = img.crop((0, 165, 1024, 1066))
+    crop.save(tmp_path)
 
-# save screenshot
-img = Image.open(io.BytesIO(br.get_screenshot_as_png()))
-crop = img.crop((0, 165, 1024, 1066))
-crop.save(tmp_path)
+    # tweet image
+    now = datetime.datetime.now()
+    date = '{t.month}/{t.day}({week}) {t.hour}:{t.minute:02d}'.format(t=now, week='月火水木金土日'[now.weekday()])
+    status = '{}現在の空席状況です♪\n予約はこちらからどうぞ↓ #プリリズカフェ\nhttp://cafereserve.animate.co.jp/store?fair_id=129'.format(date)
+    api.update_with_media(tmp_path, status=status)
 
-# tweet image
-now = datetime.datetime.now()
-date = '{t.month}/{t.day}({week}) {t.hour}:{t.minute:02d}'.format(t=now, week='月火水木金土日'[now.weekday()])
-status = '{}現在の空席状況です♪\n予約はこちらからどうぞ↓ #プリリズカフェ\nhttp://cafereserve.animate.co.jp/store?fair_id=129'.format(date)
-api.update_with_media(tmp_path, status=status)
+if __name__ == '__main__':
+    # parse args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--debug', action='store_true')
+    args = parser.parse_args()
+
+    if args.debug:
+        api = get_api('sakuramochi_pre')
+    else:
+        api = get_api('prism_cafe_bot')
+    br = PhantomJS()
+
+    main()
